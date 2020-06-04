a6604e5d442c1ec0bf0a5a4751469abb02950859
Furkan Tezeren <emintezeren@gmail.com>
2020-06-04 10:58:01 +0300

    Rebuilt and adjusted landing page for mobile

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navbar } from './Navbar';
 import { Content } from './Content';
 import '../../styles/Home.scss';
-import '../../styles/Mobile.scss'
 
 export const Home = () => {
   return (
